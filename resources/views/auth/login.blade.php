1bf20fabd4ef0e0e327aeae35b230991f65c8dfd
thuantv414 <tranthuan142004@gmail.com>
2024-09-22 12:19:09 +0700

    giao dien login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                 <div class="text-center mt-sm-5 mb-4 text-white-50">
                     <div>
                         <a href="index.html" class="d-inline-block auth-logo">
-                            <img src="{{ asset('assets/images/logo-light.png')}}" alt="" height="20">
+                            <img src="{{('assets/images/logo-light.png')}}" alt="" height="20">
                         </a>
                     </div>
                     <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
@@ -30,11 +30,11 @@
                             <p class="text-muted">Sign in to continue to Velzon.</p>
                         </div>
                         <div class="p-2 mt-4">
-                            <form action="{{route('login')}}" method="POST">
-                                @csrf
+                            <form action="index.html">
+
                                 <div class="mb-3">
                                     <label for="username" class="form-label">Username</label>
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter username">
+                                    <input type="text" class="form-control" id="username" placeholder="Enter username">
                                 </div>
 
                                 <div class="mb-3">
@@ -43,7 +43,7 @@
                                     </div>
                                     <label class="form-label" for="password-input">Password</label>
                                     <div class="position-relative auth-pass-inputgroup mb-3">
-                                        <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="passwor" name="password">
+                                        <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="password-input">
                                         <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                     </div>
                                 </div>
